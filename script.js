3db8a46c34723fd65b7085f47dc4bb83d6b9c002
Eshi44 <e.hameliervolino@gmail.com>
2020-03-12 18:24:39 -0400

    used the .ready() method and tested via console.log

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//CHeck to make sure the relative path in our html file works
+//Check to make sure the relative path in our html file works
 //console.log("Hello World");
+//Use the ready() method and test via console.log
+$(document).ready(function() {
+  console.log("Hello World");
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
